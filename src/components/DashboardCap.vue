377d2744f267d22c5f863c4be8f23220a84ca59d
Jean Everton <jeanever39@gmail.com>
2023-02-07 01:01:28 -0300

    responsividade

--- src/components/DashboardCap.vue
+++ src/components/DashboardCap.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="container">
-    <div class="container-texto">
-      <p v-if="transcript">{{ transcript }}</p>
-      <div class="container-inicio" v-else>
-        <p>Olá, vamos lá...</p>
-        <img src="/assets/img/falarbrancogrande.png" alt="incio">
+  <div class="div-container">
+    <section class="container">
+      <div class="container-texto">
+        <p class="texto-capturado" v-if="transcript">{{ transcript }}</p>
+        <div v-if="transcript" id="copy" class="div-copiar-texto" @click="copiarText">
+          <img src="/assets/img/copy.png" alt="copiar texto">
+          <span class="span-copiado none">Copiado</span>
+        </div>
+        <div class="container-inicio" v-else>
+          <p>Olá, vamos lá...</p>
+          <img src="/assets/img/falarbrancogrande.png" alt="incio">
+        </div>
       </div>
-    </div>
-    <div class="container-botoes">
-      <button @click="startRecording"
-              title="clique aqui para iniciar captura de voz">
-        Iniciar captura
-        <img class="img-botoes" src="/assets/img/falarbranco.png">
-      </button>
-      <button @click="pauseRecording" title="clique aqui para pausa captura">
-        Pause
-        <img class="img-botoes" src="/assets/img/pausabranco.png">
-      </button>
-      <button @click="resumeRecording" title="clique aqui para voltar a captura">
-        Resume
-        <img class="img-botoes" src="/assets/img/continuobranco.png">
-      </button>
-      <button @click="stopRecording" title="clique aqui para apagar texto">
-        Apagar
-        <img class="img-botoes" src="/assets/img/lixobranco.png">
-      </button>
-    </div>
+      <div class="container-botoes">
+        <button @click="startRecording"
+                title="clique aqui para iniciar captura de voz">
+          Iniciar captura
+          <img class="img-botoes" src="/assets/img/falarbranco.png">
+        </button>
+        <button @click="pauseRecording" title="clique aqui para pausa captura">
+          Pause
+          <img class="img-botoes" src="/assets/img/pausabranco.png">
+        </button>
+        <button @click="resumeRecording" title="clique aqui para voltar a captura">
+          Resume
+          <img class="img-botoes" src="/assets/img/continuobranco.png">
+        </button>
+        <button @click="stopRecording" title="clique aqui para apagar texto">
+          Apagar
+          <img class="img-botoes" src="/assets/img/lixobranco.png">
+        </button>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -70,6 +76,22 @@ export default {
       this.recognition.stop();
       this.isRecording = false;
       this.transcript = "";
+    },
+    copiarText() {
+      const input = document.createElement('input');
+      input.setAttribute('value', this.transcript);
+      document.body.appendChild(input);
+      input.select();
+      document.execCommand('copy');
+      document.body.removeChild(input);
+
+      let spanCp = document.querySelector('.span-copiado')
+      spanCp.classList.remove('none')
+
+      setInterval(()=> {
+        spanCp.classList.add('none')
+      }, 3000)
+
     }
   }
 };
